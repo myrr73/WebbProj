58a3caff9cc9fedbda124f9ff0aaf1bbc57c424c
Tobbe Myrander <tobias@myrander.se>
2018-11-25 23:08:00 +0100

    småfix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Hem</router-link> |
+      <router-link to="/butik">Butik</router-link> |
+      <router-link to="/omoss">Om Oss</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Hem from './views/Hem.vue'
+import OmOss from './views/OmOss.vue'
+import Butik from './views/Butik.vue'
 
 Vue.use(Router)
 
@@ -9,17 +11,18 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Hem
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    }
+      path: '/OmOss',
+      name: 'info',
+      component: OmOss
+    },
+    {
+      path: '/Butik',
+      name: 'Shop',
+      component: Butik
+    },
+    
   ]
 })
